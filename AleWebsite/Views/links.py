dd18d311cd4fcfa223091453af66ebd301fb036f
Alexis Cao <alexis.cao@alu.frlp.utn.edu.ar>
2024-06-10 20:00:18 -0300

    agregando el link a el repositorio de github del proyecto

--- AleWebsite/Views/links.py
+++ AleWebsite/Views/links.py
@@ -11,6 +11,7 @@ def links() -> rx.Component:
         button_link("Docker example", "https://github.com/AleeCao/Practicas_Docker", "/icons/docker.svg"),
         button_link("NodeJS example", "https://github.com/AleeCao/Login--Nodejs-MySQL-", "/icons/node.svg"),
         button_link("Python example", "https://github.com/AleeCao/TPFinal", "/icons/python.svg"),
+        button_link("Website code", "https://github.com/AleeCao/Links-Website", "/icons/web.svg"),
         rx.heading("Contact", font_size=Size.BMEDIUM.value, padding_top= Size.MEDIUM.value),
         button_link("Email", "mailto:ale.9c9gmail.com", "/icons/email.svg"),
         width="100%",
